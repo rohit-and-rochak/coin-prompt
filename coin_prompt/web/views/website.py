7312c6936a7dbec556a8090dbd190f3888ecd198
Rohit Sharma <rohitsdec4@gmail.com>
2021-11-30 15:20:12 +0530

    modified html templates and view for home

--- coin_prompt/web/views/website.py
+++ coin_prompt/web/views/website.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+import requests
+import locale
 
+from django.shortcuts import render
+from django.conf import settings
 
 def home(request):
-    return render(request, 'home.html')
+    endpoint = settings.WAZIRX_API_ALL
+    response = requests.get(endpoint)
+    coins = {}
+    if response.ok:
+        for coin in response.json():
+            if coin['quoteAsset'] == 'inr':
+                name = coin['baseAsset']
+                change = round((float(coin['lastPrice']) - float(coin['openPrice'])) / float(coin['openPrice']), 3)
+                price = coin['lastPrice']
+
+                coins[name.upper()] = {'price': price, 'change': change, 'icon': 'images/icons/{}.png'.format(name)}
+
+    return render(request, 'home.html', {'coins': coins, 'generic_icon': 'images/icons/generic.png'})
