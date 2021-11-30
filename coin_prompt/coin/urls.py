13b4c43fc55c1f54d59b3fd81dc1de8d9ff46b5b
Rohit Sharma <rohitsdec4@gmail.com>
2021-12-01 03:34:14 +0530

    add code to delete alert

--- coin_prompt/coin/urls.py
+++ coin_prompt/coin/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import create_alert
+from .views import create_alert, delete_alert
 
 urlpatterns = [
-    path('create_alert/', create_alert, name="create-alert")
+    path('create_alert/', create_alert, name="create-alert"),
+    path('delete_alert/', delete_alert, name="delete-alert")
 ]
--- coin_prompt/coin/views.py
+++ coin_prompt/coin/views.py
@@ -23,4 +23,20 @@ def create_alert(request):
         else:
             return JsonResponse({'error': f'Coin {data.get("coin")} not found'}, status=500)
 
-    return JsonResponse({'error': 'Invalid Response'}, status=500)
+    return JsonResponse({'error': 'Invalid Request'}, status=500)
+
+
+@require_POST
+@login_required
+def delete_alert(request):
+    if request.is_ajax:
+        data = request.POST
+        user = request.user
+        alert = Alert.objects.filter(user=user, coin__name=data.get('coin')).first()
+        if alert:
+            alert.delete()
+            return JsonResponse({'data': {}}, status=200)
+        else:
+            return JsonResponse({'error': f'Coin {data.get("coin")} not found'}, status=500)
+
+    return JsonResponse({'error': 'Invalid request'}, status=500)
