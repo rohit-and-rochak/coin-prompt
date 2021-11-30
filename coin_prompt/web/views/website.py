9f3da140595370350163cd6cfd9fd609fbd2d69c
Rohit Sharma <rohitsdec4@gmail.com>
2021-11-30 22:00:11 +0530

    add alerts in the context for home view

--- coin_prompt/web/views/website.py
+++ coin_prompt/web/views/website.py
@@ -1,8 +1,10 @@
 import requests
-import locale
 
 from django.shortcuts import render
 from django.conf import settings
+
+from coin.models import Alert
+
 
 def home(request):
     endpoint = settings.WAZIRX_API_ALL
@@ -14,7 +16,12 @@ def home(request):
                 name = coin['baseAsset']
                 change = round((float(coin['lastPrice']) - float(coin['openPrice'])) / float(coin['openPrice']), 3)
                 price = coin['lastPrice']
-
                 coins[name.upper()] = {'price': price, 'change': change, 'icon': 'images/icons/{}.png'.format(name)}
 
-    return render(request, 'home.html', {'coins': coins, 'generic_icon': 'images/icons/generic.png'})
+    context = {'coins': coins}
+
+    if request.user.is_authenticated:
+        alerts = Alert.objects.filter(user=request.user)
+        context['alerts'] = alerts
+
+    return render(request, 'home.html', context)
